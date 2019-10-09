c1e4d8df4ec53edd7f60e8ea8c0b1dac6968b8f1
Duong Dang <dddangmedia@gmail.com>
2019-10-09 18:57:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,39 @@
-def consolidate_cart(cart)
-  # code here
+require 'pry'
+
+def consolidate_cart(array)
+  cart = {}
+  array.each_with_index do |item, i|
+    item.each do |food, info|
+      if cart[food]
+        cart[food][:count] += 1
+      else
+        cart[food] = info
+        cart[food][:count] = 1
+      end
+    end
+  end
+  cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
-end
-
-def apply_clearance(cart)
-  # code here
-end
-
-def checkout(cart, coupons)
-  # code here
+  item = coupon[:item]
+  coupons.each do |coupon|
+    if cart.keys.include?(coupon[:item]) 
+      binding.pry
+      if cart[item][:count] >= coupon[:num] && !cart["#{item} W/COUPON"]
+        cart["#{cart.keys} W/COUPON"] = {
+          :price => coupon[:price]
+        }
+         
+      else
+        cart["#{cart.keys} W/COUPON"] = {
+          :price => coupon[:cost] / cart.values[:count], 
+          :clearance => cart.values[:clearance], 
+          :count => coupon[:num]
+        }
+      end
+      cart[:count] -= coupon[:num]
+    end   
+  end
+  cart
 end
